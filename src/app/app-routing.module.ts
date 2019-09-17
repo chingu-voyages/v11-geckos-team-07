bf9011f2e3cf71b70f86ef636956b8e8299f103b
afmoleirinho96 <andrewacm25@gmail.com>
2019-09-17 20:42:47 +0100

    Every Url redirects to Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,8 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-    {
-      path: '',
-      redirectTo: 'home',
-      pathMatch: 'full'
-    },
-    {
-      path: 'home',
-      component: HomeComponent
-    }
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
