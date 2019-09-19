d9500f58d2d249601ee5493ff962f8cf7dce6f9b
Akram <akrammoussaoui14@gmail.com>
2019-09-20 00:31:16 +0100

    Merge pull request #27 from chingu-voyages/issue/23
    Created team component and linked withing router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
 
 
 const routes: Routes = [
-    { path: '**', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent }
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'team', component: TeamComponent }
 ];
 
 @NgModule({
