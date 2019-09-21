f9126ddd9407ae7f468e2fb7e358e83953ce95cf
Akram <akrammoussaoui14@gmail.com>
2019-09-21 21:30:06 +0100

    put the dropdown in the real-time component and changes the icon in index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RealTimeRateExchangeComponent } from './real-time-rate-exchange/real-time-rate-exchange.component';
-import { FooterComponent } from './footer/footer.component';
-import { SpecificDateRateExchangeComponent } from './specific-date-rate-exchange/specific-date-rate-exchange.component';
-import { HomeComponent } from './home/home.component';
-import { TeamComponent } from './team/team.component';
-import { DropdownCurrencyComponent } from './dropdown-currency/dropdown-currency.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { RealTimeRateExchangeComponent } from "./real-time-rate-exchange/real-time-rate-exchange.component";
+import { FooterComponent } from "./footer/footer.component";
+import { SpecificDateRateExchangeComponent } from "./specific-date-rate-exchange/specific-date-rate-exchange.component";
+import { HomeComponent } from "./home/home.component";
+import { TeamComponent } from "./team/team.component";
 
 @NgModule({
   declarations: [
@@ -20,15 +19,10 @@ import { DropdownCurrencyComponent } from './dropdown-currency/dropdown-currency
     FooterComponent,
     SpecificDateRateExchangeComponent,
     HomeComponent,
-    TeamComponent,
-    DropdownCurrencyComponent
+    TeamComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/real-time-rate-exchange/real-time-rate-exchange.component.ts
+++ src/app/real-time-rate-exchange/real-time-rate-exchange.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-real-time-rate-exchange',
-  templateUrl: './real-time-rate-exchange.component.html',
-  styleUrls: ['./real-time-rate-exchange.component.css']
+  selector: "app-real-time-rate-exchange",
+  templateUrl: "./real-time-rate-exchange.component.html",
+  styleUrls: ["./real-time-rate-exchange.component.css"]
 })
 export class RealTimeRateExchangeComponent implements OnInit {
+  currencies: string[] = ["EUR", "USA", "CZK"]; //In future it will get currencies from api
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  selectCurrency(currency: string) {
+    console.log(currency);
   }
-
 }
