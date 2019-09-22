380545086c3b851516d1787833286eb535dbba75
Akram <akrammoussaoui14@gmail.com>
2019-09-22 17:55:16 +0100

    adding EUR to the dropdown

--- src/app/services/rate-exchange.service.ts
+++ src/app/services/rate-exchange.service.ts
@@ -17,7 +17,9 @@ export class RateExchangeService {
   getAvailableCurrencies(): Observable<string[]> {
     return this.http.get<any>(`${this.url}latest`).pipe(
       map((data: any) => {
-        return Object.keys(data.rates);
+        let currenciesArray = Object.keys(data.rates);
+        currenciesArray.push("EUR");
+        return currenciesArray;
       })
     );
   }
