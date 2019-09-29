33ba2e800cfba902a0dc7597ec4270d63f98ba1a
Akram <akrammoussaoui14@gmail.com>
2019-09-29 20:43:53 +0100

    remove unnecessary request

--- src/app/real-time-rate-exchange/real-time-rate-exchange.component.ts
+++ src/app/real-time-rate-exchange/real-time-rate-exchange.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { RateExchangeService } from "../services/rate-exchange.service";
-import { stringify } from '@angular/compiler/src/util';
+import { stringify } from "@angular/compiler/src/util";
 
 @Component({
   selector: "app-real-time-rate-exchange",
@@ -13,7 +13,7 @@ export class RealTimeRateExchangeComponent implements OnInit {
   resultCurrency: string;
   amount: string;
   result: number;
-  last_updated:string;
+  last_updated: string;
 
   constructor(private rateExchangeService: RateExchangeService) {}
 
@@ -22,10 +22,6 @@ export class RealTimeRateExchangeComponent implements OnInit {
       this.currencies = data; // It initializes the dropdowns with available currencies
       this.baseCurrency = this.currencies[0];
       this.resultCurrency = this.currencies[0];
-    });
-
-    this.rateExchangeService.getExchangeRate("USD", "GBP").subscribe(data => {
-      console.log(data); // It's here just for testing, in future this will be in the calculating result function
     });
   }
 
@@ -37,21 +33,23 @@ export class RealTimeRateExchangeComponent implements OnInit {
     this.resultCurrency = currency;
   }
 
-  switchCurrency(){
-    let temp:string;
-    temp=this.baseCurrency;
-    this.baseCurrency=this.resultCurrency;
-    this.resultCurrency=temp;
+  switchCurrency() {
+    let temp: string;
+    temp = this.baseCurrency;
+    this.baseCurrency = this.resultCurrency;
+    this.resultCurrency = temp;
   }
 
-  convert(){
-    if(this.amount){
-      this.rateExchangeService.getExchangeRate(this.baseCurrency,this.resultCurrency).subscribe(data=>{
-        this.result=data.rate* parseInt(this.amount);
-        this.last_updated=data.date;
-      })
-    }else{
-      this.result=0;
+  convert() {
+    if (this.amount) {
+      this.rateExchangeService
+        .getExchangeRate(this.baseCurrency, this.resultCurrency)
+        .subscribe(data => {
+          this.result = data.rate * parseInt(this.amount);
+          this.last_updated = data.date;
+        });
+    } else {
+      this.result = 0;
     }
   }
 }
